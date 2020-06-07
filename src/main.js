7e7f958b3a9be2ce95f839f5ae6cb8f96f6864a1
leijin0416 <1427780404@qq.com>
2020-06-07 23:48:15 +0800

    上拉加载数据实例

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import filters from './filters';
 import VueWechatTitle from 'vue-wechat-title';
 import VideoPlayer from 'vue-video-player';
 import VueLazyLoad from 'vue-lazyload';
+// 轮播图
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 // 挂载图表
 import myCharts from "@/components/echarts/myCharts";
@@ -21,7 +22,10 @@ require('vue-video-player/src/custom-theme.css');
 
 Vue.config.productionTip = false; // 关闭生产模式下给出的提示
 
+Vue.use(VueWechatTitle);
 Vue.use(VueAwesomeSwiper);
+Vue.use(VideoPlayer);
+Vue.use(myCharts);
 Vue.use(VueLazyLoad, {
     preLoad: 1.3,
     attempt: 1,
@@ -29,9 +33,6 @@ Vue.use(VueLazyLoad, {
     error: require('@/assets/error.jpg'),        //报错需要的图片
     loading: require('@/assets/loadingTimg.png')	// 替换需要的图片
 });
-Vue.use(VideoPlayer);
-Vue.use(VueWechatTitle);
-Vue.use(myCharts)
 
 // 全局过滤器
 Object.keys(filters).forEach(filterName => {
--- src/views/htmls/shop/index.vue
+++ src/views/htmls/shop/index.vue
@@ -2,20 +2,27 @@
 	<div class="page v-router">
 		<div class="container">
 			<h3 class="v-h3-new">最新消息</h3>
-			<div class="v-news-box">
-				<div class="v-news-list" v-for="item in newsData" :key="item.id">
-					<div class="v-main">
-						<p class="v-time">{{item.date}}</p>
-						<p class="v-text">真实性：{{item.result}}</p>
-						<div class="v-item">
-							<h3 class="v-h3">{{item.title}}</h3>
-							<h4 class="v-h4">{{item.abstract}}</h4>
-							<p class="v-text">来源：<span>{{item.author}}</span></p>
+			<van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+				<van-list
+					v-model="loading"
+					:finished="finished"
+					finished-text="没有更多了"
+					@load="onLoadData" >
+					<div class="v-news-box">
+						<div class="v-news-list" v-for="item in newsData" :key="item.id">
+							<div class="v-main">
+								<p class="v-time">{{item.date}}</p>
+								<p class="v-text">真实性：{{item.result}}</p>
+								<div class="v-item">
+									<h3 class="v-h3">{{item.title}}</h3>
+									<h4 class="v-h4">{{item.abstract}}</h4>
+									<p class="v-text">来源：<span>{{item.author}}</span></p>
+								</div>
+							</div>
 						</div>
-
 					</div>
-				</div>
-			</div>
+				</van-list>
+			</van-pull-refresh>
 		</div>
 	</div>
 </template>
@@ -33,16 +40,19 @@ export default {
 	},
 	data() {
 		return {
+			loading: false,
+			finished: false,
+			refreshing: false,
 			newsData: [],
-			data: {
-				page: 1,
-			},
+            pages: {
+                pageSize: 0,
+                pageNum: 1
+            },
 			bg: require('@/assets/img/hk/logo.png'),
 		}
 	},
 	//页面初始化
 	mounted(){
-		this.fetchNewsData()
 	},
 	//监听click方法
 	methods: {
@@ -50,26 +60,55 @@ export default {
 		 * 	对于上拉加载更多
 		 *  需要手动进行分页
 		 */
-        fetchNewsData () {
+		onRefresh() {
 			let _that = this;
-			let {page, num} = _that.data;
+			// 清空列表数据
+			_that.finished = false;
+			// 重新加载数据
+			// 将 loading 设置为 true，表示处于加载状态
+			_that.loading = true;
+            _that.pages.pageSize = 0;
+			_that.onLoadData();
+		},
+		/**
+		 * 	数据立即加载
+		 * 	-控制上拉加载
+		 */
+		onLoadData() {
+			let _that = this;
+			let {pageSize, pageNum} = _that.pages;
 
             ajWebGetSendChinaNews({
-				'page': page
+				'page': pageSize + 1
             }).then( res => {
-                // let data = JSON.parse(res.data.data);
+				if (_that.refreshing) {
+					_that.newsData = [];
+					_that.refreshing = false;
+				}
+				// let data = JSON.parse(res.data.data);
+
+				// let lengths = res.data.result.data.pages;  // 总条数
+                // let pageSize = res.data.result.data.pageSize;	// 当前页码
 				let data = res.data.data;
-				_that.newsData = data;
-                // console.log(res)
-            })
-        },
+				 _that.newsData = data;
+				_that.loading = false;
+				// _that.pages.pageSize = pageSize;
+
+				if (_that.newsData.length >= data.length) {
+					_that.finished = true;
+				}
+                console.log(res);
+			})
+
+		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
+/deep/.van-pull-refresh {padding: 0 10px;}
 .page {
-	padding: 0 30px 80px;
+	padding: 0 20px 80px;
 	.v-h3-new {
 		padding: 30px 0;
 		font-size: 36px;
