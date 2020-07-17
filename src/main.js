2df91ea3f1b4f42f744a83f5fc111ca8de6fce36
leijin0416 <1427780404@qq.com>
2020-07-18 00:22:42 +0800

    Vue 公共文档

--- src/main.js
+++ src/main.js
@@ -5,26 +5,21 @@ import store from './store';
 import i18n from './locale';
 import filters from './filters';
 
-import VueWechatTitle from 'vue-wechat-title';
-import VideoPlayer from 'vue-video-player';
-import VueLazyLoad from 'vue-lazyload';
-// 轮播图
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-// 挂载图表
-import myCharts from "@/components/echarts/myCharts";
+import VueWechatTitle from 'vue-wechat-title';          // title
+import myCharts from "@/components/echarts/myCharts";  // 挂载图表
+import VueLazyLoad from 'vue-lazyload';     // 图片懒加载
 
-import 'swiper/css/swiper.css';
+import VideoPlayer from 'vue-video-player';
+import VueAwesomeSwiper from 'vue-awesome-swiper'; // 轮播图
+
 import "./style/reset.scss";
-import "./style/animation.scss";
 import "./style/common.scss";
 
+import 'swiper/css/swiper.css';
+import "./style/animation.scss";
 require('vue-video-player/src/custom-theme.css');
 
-Vue.config.productionTip = false; // 关闭生产模式下给出的提示
-
 Vue.use(VueWechatTitle);
-Vue.use(VueAwesomeSwiper);
-Vue.use(VideoPlayer);
 Vue.use(myCharts);
 Vue.use(VueLazyLoad, {
     preLoad: 1.3,
@@ -33,6 +28,10 @@ Vue.use(VueLazyLoad, {
     error: require('@/assets/error.jpg'),        //报错需要的图片
     loading: require('@/assets/loadingTimg.png')	// 替换需要的图片
 });
+Vue.use(VueAwesomeSwiper);
+Vue.use(VideoPlayer);
+
+Vue.config.productionTip = false; // 关闭生产模式下给出的提示
 
 // 全局过滤器
 Object.keys(filters).forEach(filterName => {
