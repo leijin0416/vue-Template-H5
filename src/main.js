6d17ae1308d1b0a60a8da4c368b308092087081b
leijin0416 <1427780404@qq.com>
2020-08-23 23:53:23 +0800

    文档整理

--- src/main.js
+++ src/main.js
@@ -8,17 +8,13 @@ import filters from './filters';
 import VueWechatTitle from 'vue-wechat-title';          // title
 import myCharts from "@/components/echarts/myCharts";   // 挂载图表
 import VueLazyLoad from 'vue-lazyload';                 // 图片懒加载
-
-import VideoPlayer from 'vue-video-player';
-import VueAwesomeSwiper from 'vue-awesome-swiper';      // 轮播图
+import VideoPlayer from 'vue-video-player';             // 视频
 
 import "./style/reset.scss";
 import "./style/common.scss";
-
-import 'swiper/css/swiper.css';
 import "./style/animation.scss";
-require('vue-video-player/src/custom-theme.css');
 
+require('vue-video-player/src/custom-theme.css');
 Vue.use(VueWechatTitle);
 Vue.use(myCharts);
 Vue.use(VueLazyLoad, {
@@ -28,21 +24,17 @@ Vue.use(VueLazyLoad, {
     error: require('@/assets/error.jpg'),           // 报错需要的图片
     loading: require('@/assets/loadingTimg.png')	// 替换需要的图片
 });
-
 Vue.use(VideoPlayer);
-Vue.use(VueAwesomeSwiper);
-
 Vue.config.productionTip = false;       // 关闭生产模式下给出的提示
 
 // 全局过滤器
 Object.keys(filters).forEach(filterName => {
     Vue.filter(filterName, filters[filterName])
 })
-
 /* eslint-disable no-new */
 new Vue({
     router,
     store,
     i18n,
     render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
